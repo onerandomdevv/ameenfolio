57107d22755ad79657b6c0f5901336ab3704d7ad
AliameenXBT <abdulkareemalameen75@gmail.com>
2026-08-13 22:39:55 +0100

    fix(writing): serve reliable static social cards

--- src/app/writing/[slug]/twitter-image.tsx
+++
@@ -1,6 +0,0 @@
-export { default } from "./opengraph-image";
-
-export const alt = "Article preview card";
-export const dynamic = "force-dynamic";
-export const size = { width: 1200, height: 630 };
-export const contentType = "image/png";
